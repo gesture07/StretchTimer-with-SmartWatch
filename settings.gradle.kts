3d5d8bf479efe8d9307ef8b38e265749b349a4e4
YoonDaHye <1130emma@naver.com>
2025-01-12 15:22:18 +0900

    프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StretchTimer"
+include(":mobile")
+include(":wear")
+ 
